93d38362291429e4f0f02a0bd93f4d1e0b9a011a
elotrolla <elo.trolla@khk.ee>
2015-10-28 10:19:43 +0200

    4. Navigeerimatu link [Delivers #105991226]

--- kliendipoolsed.php
+++ kliendipoolsed.php
@@ -2,13 +2,17 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Trolla</title>
+    <title>Pseudoprjekt_VS15_EloTrolla</title>
 </head>
 <body>
 
 <button type="button" onclick="alert('Tere maailm!')">Tere maailm!</button><br>
 
-<a href="http://www.khk.ee" target="_blank" onclick="alert('Tere maailm!')">Tere maailm!</a>
+<a href="http://www.khk.ee" target="_blank" onclick="alert('Tere maailm!')">Tere maailm!</a><br>
+
+<a href="khk.ee" onclick="alert('Jääme siia!'); return false;">Jääme siia!</a>
+
+
 
 </body>
 </html>
