f7081639581dd0ccd8694b9c3b7f4db9b3325e6e
elotrolla <elo.trolla@khk.ee>
2015-10-28 11:16:35 +0200

    5. Kass koeraks [Delivers #105991228]

--- kliendipoolsed.php
+++ kliendipoolsed.php
@@ -7,11 +7,32 @@
 <body>
 
 <button type="button" onclick="alert('Tere maailm!')">Tere maailm!</button><br>
+<br>
 
 <a href="http://www.khk.ee" target="_blank" onclick="alert('Tere maailm!')">Tere maailm!</a><br>
+<br>
 
-<a href="khk.ee" onclick="alert('Jääme siia!'); return false;">Jääme siia!</a>
+<a href="khk.ee" onclick="alert('Jääme siia!'); return false;">Jääme siia!</a><br>
+<br>
 
+<div>
+
+    <img id="minupilt" onclick="changeImage()" src="kama/kass.png">
+
+    <p>Vajuta kassi pildile, et ilmuks koera pilt. Või vastupidi.</p>
+
+    <script>
+        function changeImage() {
+            var image = document.getElementById('minupilt');
+            if (image.src.match("kass")) {
+                image.src = "kama/koer.png";
+            } else {
+                image.src = "kama/kass.png";
+            }
+        }
+    </script>
+
+</div>
 
 
 </body>
