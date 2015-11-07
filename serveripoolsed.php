0f6026dba69183d3d7e96d1e4e008fd3c5ec7be2
elotrolla <elo.trolla@khk.ee>
2015-11-07 20:18:33 +0200

    18. Objektid [Delivers #105991256]

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -6,23 +6,52 @@ $vanus = 123;
 $eesnimi_algab_vokaaliga = true;
 
 
-$isik = array(
-    "eesnimi" => "Elo",
-    "perenimi" => "Trolla",
-    "vanus" => "123"
-
-);
-
-echo "$isik[eesnimi] $isik[perenimi] ($isik[vanus])<br>";
 
 
-if(preg_match( "/^[aeiouõäöü]/i", $eesnimi))     /* 'i' - ei tee vahet suur- ja v'iket'htedel;
-                                                '^' - j'rgnevad t'hed on rea/stringi alguses; */
- {
+
+// Objektid, klassid
+
+class Person //Klass on nagu sabloon, vorm
+{
+
+    var $eesnimi;
+    var $perenimi;
+    var $vanus;
+    var $sugu;
+
+}
+
+$isik = new Person();
+$isik->eesnimi = "Elo";
+$isik->perenimi = "Trolla";
+$isik->vanus = 123;
+$isik->sugu = "Naine";
+
+/*   Need on objektid ($isik, $isik2)
+
+$isik2 = new Person();
+$isik2->eesnimi = "Vares";
+$isik2->perenimi = "Vaaks";
+$isik2->vanus = 11;
+$isik2->sugu = "Mees";
+*/
+
+echo $isik->eesnimi.' '.$isik->perenimi.' '.'('.$isik->vanus.')'.' '.$isik->sugu.'<br>';
+
+
+
+
+
+// Nimi algab/ei alga nt. vokaaliga
+
+if (preg_match("/^[a, e, i, o, u, õ, ä, ö, ü]/i", $eesnimi))     /* 'i' - ei tee vahet suur- ja v'iket'htedel;
+                                                                    '^' - j'rgnevad t'hed on rea/stringi alguses; */
+{
     echo "Eesnimi algab vokaaliga";
 }
 
-else {
+else
+{
     echo "Eesnimi ei alga vokaaliga";
 }
 
