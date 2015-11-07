709235b55b4f900b58cc701b6e6270b1146693eb
elotrolla <elo.trolla@khk.ee>
2015-11-07 16:44:08 +0200

    17. Tingimused [Delivers #105991254]

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -13,6 +13,17 @@ $isik = array(
 
 );
 
-echo "$isik[eesnimi] $isik[perenimi] ($isik[vanus])";
+echo "$isik[eesnimi] $isik[perenimi] ($isik[vanus])<br>";
+
+
+if(preg_match( "/^[aeiouõäöü]/i", $eesnimi))     /* 'i' - ei tee vahet suur- ja v'iket'htedel;
+                                                '^' - j'rgnevad t'hed on rea/stringi alguses; */
+ {
+    echo "Eesnimi algab vokaaliga";
+}
+
+else {
+    echo "Eesnimi ei alga vokaaliga";
+}
 
 ?>
